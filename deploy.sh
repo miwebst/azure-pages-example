e6e5c4f982eb4cd651a807fd466e29b6a6c32db7
Wassim Chegham <github@wassim.dev>
2020-02-05 18:09:24 +0100

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-RESOURCE="azure_pages"
-ARM_TEMPLATE="staticSitesPayload.json"
-az group create --name $RESOURCE --location "westus"
+RESOURCE="azure_static_sites_demo_westus2"
+ARM_TEMPLATE_FILE="staticSitesPayload.json"
+SUBSCRIPTION_ID="198cd413-7754-4cae-b7b4-048151e7bb79"
+ARM_URL="https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE/providers/Microsoft.Web/staticSites/armtest0?api-version=2019-12-01-preview"
 
-az group deployment validate \
-  --resource-group $RESOURCE \
-  --template-file $ARM_TEMPLATE
+# Create a temporary resource group
+# az group create --name $RESOURCE --location "westus"
 
+# az rest \
+#   --debug --verbose \
+#   --method get \
+#   --uri "$ARM_URL"
+
+echo ">> deploying to $ARM_URL"
+az rest \
+  --method put \
+  --uri "$ARM_URL" \
+  --body "@$ARM_TEMPLATE_FILE" 
